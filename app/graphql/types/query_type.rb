3edbacf8cd82d6b51aa45f90d6e80b7da178438c
ncleguizamon <raiis>
2019-03-29 16:50:03 -0500

    configuracòn  de authores con los libros

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,49 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+    field :test_field, String, null: false,
+      description: "An example field added by the generator"
+    def test_field
+      "Hello World!"
+    end
+
+    
+    field :authors, [Types::AuthorType], null: false do
+      description 'Find all authors'
+    end
+
+    field :author, Types::AuthorType, null: false do
+      description 'Find a author by ID'
+      argument :id, ID, required: true
+    end
+
+    field :books, [Types::BookType], null: false do
+      description 'Find all authors'
+    end
+
+    field :book, Types::BookType, null: false do
+      description 'Find a author by ID'
+      argument :id, ID, required: true
+    end
+
+    def authors
+      Author.all
+    end
+
+    def author(id:)
+      Author.find(id)
+    end
+
+    def books
+      Book.all
+    end
+
+    def book(id:)
+      Book.find(id)
+    end
+   
+  end
+end
